0d1eed4ecf8823b486b468f9d130bb0fac13feff
ayano-yuki <kd0ayano@gmail.com>
2025-10-11 14:09:46 +0900

    init

---
+++ vivliostyle.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'mybook',
+  author: 'イエナリ',
+  size: 'A5',
+  language: 'ja',
+  theme: './theme/theme.css',
+  toc: true,
+  tocTitle: '目次', 
+  
+  entry: [
+    'example.md',
+  ],
+  entryContext: './content',
+};
